2fbf909aa8dc55c3b06ddf3126b5d6198c32c787
Maciej <maciej@haply.co>
2022-12-01 12:16:11 -0500

    Added basic command line user inteface package

---
+++ cmi.py
@@ -0,0 +1,42 @@
+class CMI:
+    
+    selection_out_of_range_message = "Selection out of range. Please try again."
+    selection_not_number = "Please enter a number. Please try again."
+    
+    cancel_command = "cnc"
+        
+    def __init__(self):
+        pass
+    
+    def select_option(self, options, tabs = 0):
+        cc = 0
+        for option in options:
+            print("\t"*tabs + str(cc) +" - " + option)
+            cc += 1
+        return self.get_selection(cc, tabs)
+            
+    def get_selection(self, cc, tabs):
+        while True:
+            selection = input("\n\t"*(tabs) + "Please select an option: ")
+            if selection.isnumeric():
+                if int(selection) <= cc:
+                    return int(selection)
+                else:
+                    print("\t"*(tabs+1) + self.selection_out_of_range_message)
+            elif selection == self.cancel_command:
+                return "cnc"
+            else:
+                print("\t"*(tabs+1) + self.selection_not_number)
+    
+    def get_numeric_input(self, message, tabs = 0):
+        while True:
+            selection = input("\t"*(tabs) + message)
+            if selection.isnumeric():
+                return int(selection)
+            elif selection == self.cancel_command:
+                return "cnc"
+            else:
+                print("\t"*(tabs+1) + self.selection_not_number)
+    
+    def string_input(self, message, tabs = 0):
+        return input("\t"*(tabs) + message)
